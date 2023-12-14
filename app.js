66d0128a65ec77cda9c59c8e5f73d6031e55bcc4
Kobe Wang <123610737+kwwwv@users.noreply.github.com>
2023-12-14 14:57:40 -0500

    removed everything and left skeleton

--- app.js
+++ app.js
@@ -21,21 +21,6 @@ const database_and_collection = { db: database, collection: collection };
 const uri = `mongodb+srv://${userName}:${password}@cluster0.t8eetqo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
-});
-
-
-
-const upload = multer({
-    storage: storage,
-    limits: { fileSize: 20 * 1024 * 1024 }, 
-});
 
 app.set("views", path.resolve(__dirname, "pages"));
 app.set("view engine", "ejs");
@@ -70,48 +55,7 @@ process.stdin.on('readable', () => {
 
 
 app.get("/", (req, res) => {
-    res.render("getting", {apiKey: apiKey});
-});
-
-
-app.post("/home", async (req, res) => {
-
-    const link = req.body;
-
-    res.render("home", {
-        link: link
-    });
-    try {
-        await client.connect();
-        await client.db(database_and_collection.db).collection(database_and_collection.collection).insertOne({ link: link });
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-
-});
-
-
-app.post("/video", upload.single("videoFile"), async (req, res) => {
-    console.log(req);
-    try {
-        if (!req.file) {
-            throw new Error('No file uploaded.');
-        }
-
-        const video = req.file.path;
-        const videoPlayer = `<video width="640" height="360" controls>
-            <source src="${video}" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>`;
-
-        res.render("videoPlayer", { video: videoPlayer });
-    } catch (error) {
-        console.error('Error in /video route:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    res.render("index");
 });
 
 
